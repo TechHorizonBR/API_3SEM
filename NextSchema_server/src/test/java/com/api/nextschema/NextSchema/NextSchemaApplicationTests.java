ce597db7f802c67a42f8e72e2f22dacd345f5a2f
santosjhony12 <santosdesouzajhony@gmail.com>
2024-03-18 21:53:00 -0300

    :tada: Add new server project

---
+++ NextSchema_server/src/test/java/com/api/nextschema/NextSchema/NextSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.nextschema.NextSchema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
