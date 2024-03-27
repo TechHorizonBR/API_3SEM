a616ebfdebf3250364687b14d6b5703ed9f6a82d
vitoriabrancatti <Vitoria.Batista@br.nsg.com>
2024-03-27 12:43:00 -0300

    :refactor: NS-57

--- NextSchema_server/src/database/dbnextschema-2.sql
+++
@@ -1,9 +0,0 @@
-create database dbnextschema;
-
-/*CRIANDO USUÁRIO*/
-create user 'devs'@'localhost' identified by 'password123';
-
-/*CONCEDENDO PERMISÃO*/
-grant all privileges on *.* to 'devs'@'localhost';
-GRANT CREATE, UPDATE, DROP, DELETE, INSERT, SELECT ON dbnextschema.* TO 'devs'@'localhost';
-
