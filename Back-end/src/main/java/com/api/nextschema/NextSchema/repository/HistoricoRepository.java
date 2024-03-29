073527a8b6251ef12f3244ba377647908afe7325
santosjhony12 <santosdesouzajhony@gmail.com>
2024-03-29 18:08:29 -0300

    :feat: NS-65 Implementando Service Histórico

--- Back-end/src/main/java/com/api/nextschema/NextSchema/repository/HistoricoRepository.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/repository/HistoricoRepository.java
@@ -1,9 +1,16 @@
 package com.api.nextschema.NextSchema.repository;
 
 import com.api.nextschema.NextSchema.entity.Historico;
+import com.api.nextschema.NextSchema.entity.Metadata;
+import com.api.nextschema.NextSchema.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HistoricoRepository extends JpaRepository<Historico, Long> {
+    List<Historico> findByUser(Usuario usuario);
+
+    List<Historico> findByMetada(Metadata metadata);
 }
