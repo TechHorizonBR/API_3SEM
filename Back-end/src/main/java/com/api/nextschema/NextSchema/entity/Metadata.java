27e87b6f8b12fee1bcdc0a4f0973c04baf94753c
Luis Guimarães <luisegsilva.le@gmail.com>
2024-03-31 03:02:35 -0300

    :refactor: NS-52 update classe Metadata

--- Back-end/src/main/java/com/api/nextschema/NextSchema/entity/Metadata.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/entity/Metadata.java
@@ -1,35 +1,22 @@
 package com.api.nextschema.NextSchema.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Table(name = "metadata")
 @Getter
 @Setter
 @Entity
-public class Metadata {
-    private String nomeArquivo;
-    private String email;
+@NoArgsConstructor
+@AllArgsConstructor
 
+public class Metadata {
+    @Column(name = "nome")
+    private String nome;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    public Metadata(String nomeArquivo, Integer id) {
-        this.nomeArquivo = nomeArquivo;
-        this.id = id;
-    }
-
-    public Metadata(String nomeArquivo, String email, Integer id) {
-        this.nomeArquivo = nomeArquivo;
-        this.email = email;
-        this.id = id;
-    }
-
-    public Metadata() {
-
-    }
 }
