dcecbc58cf02f87561aadc7eb35014f762bbaaa6
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-04-03 20:40:50 -0300

    :feat: NS-57 inserindo atributos a classe ColunaResponseDto

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/dto/ColunaResponseDto.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/dto/ColunaResponseDto.java
@@ -1,5 +1,9 @@
 package com.api.nextschema.NextSchema.web.dto;
 
 public class ColunaResponseDto {
+    private String nome;
+    private String tipo;
+    private String descricao;
+    private String restricao;
 
 }
