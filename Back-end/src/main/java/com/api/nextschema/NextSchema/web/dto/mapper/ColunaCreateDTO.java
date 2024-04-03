69ab04d10234af44adb041d2ebeed0b4cadafe47
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-04-03 20:23:13 -0300

    :feat: NS-57 inserindo métodos

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/dto/mapper/ColunaCreateDTO.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/dto/mapper/ColunaCreateDTO.java
@@ -1,5 +1,11 @@
 package com.api.nextschema.NextSchema.web.dto.mapper;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class ColunaCreateDTO {
     private String nome;
     private String tipo;
