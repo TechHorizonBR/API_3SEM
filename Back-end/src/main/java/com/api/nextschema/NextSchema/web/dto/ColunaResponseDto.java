96942d0c45f6e1c2d24e0961f018f06f1af5d502
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-04-03 20:41:47 -0300

    :feat: NS-57 adicionando métodos

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/dto/ColunaResponseDto.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/dto/ColunaResponseDto.java
@@ -1,5 +1,12 @@
 package com.api.nextschema.NextSchema.web.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class ColunaResponseDto {
     private String nome;
     private String tipo;
