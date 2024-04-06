28ea43c85143a81f300635ee75a5b2f9989b562b
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-04-05 22:18:19 -0300

    :feat: NS-57 adicionando anotação

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/EmpresaController.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/EmpresaController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/empresas")
+@CrossOrigin("*")
 
 public class EmpresaController {
     private final EmpresaService empresaService;
