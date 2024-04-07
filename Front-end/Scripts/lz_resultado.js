bfeb154f959bb58daa5b9540ec63f7a95bf3af2f
Amanda Vannucci <vannucci_job@hotmail.com>
2024-04-07 15:48:27 -0300

    :feat: NS - 74 atualização de tabelas e requisicao.

---
+++ Front-end/Scripts/lz_resultado.js
@@ -0,0 +1,38 @@
+async function teste() {
+    try {
+        let response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        console.log(response.status);
+        popularTabela(response.data);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+teste()
+console.log("oi")
+
+function popularTabela(dados_Json) {
+    let tabela = document.getElementById('tabela').getElementsByTagName('tbody')[0];
+
+    for (let x = 0; x < dados_Json.length; x++) {
+        let linha = tabela.insertRow();
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        let celulaCheck = linha.insertCell(0);
+        let celulaId = linha.insertCell(1);
+        let celulaRequisicao = linha.insertCell(2);
+        let celulaBody = linha.insertCell(3);
+        celulaCheck.appendChild(checkbox);
+        celulaId.innerHTML = dados_Json[x].id;
+        celulaRequisicao.innerHTML = dados_Json[x].title;
+        celulaBody.innerHTML = dados_Json[x].body;
+
+        celulaCheck.classList.add("tab_check");
+
+
+    }
+
+}
+
