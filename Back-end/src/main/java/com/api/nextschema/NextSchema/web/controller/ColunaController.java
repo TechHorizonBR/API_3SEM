81ebdf08cbb07db72927f872d489894d68d3301f
santosjhony12 <santosdesouzajhony@gmail.com>
2024-04-09 22:08:11 -0300

    :feat: NS-67 subindo configuração de documentação de endpoint

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/ColunaController.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/ColunaController.java
@@ -7,6 +7,11 @@ import com.api.nextschema.NextSchema.service.ColunaService;
 import com.api.nextschema.NextSchema.web.dto.ColunaCreateDto;
 import com.api.nextschema.NextSchema.web.dto.ColunaResponseDto;
 import com.api.nextschema.NextSchema.web.dto.mapper.ColunaMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +25,18 @@ import java.util.List;
 @RequestMapping("/colunas")
 @RequiredArgsConstructor
 @CrossOrigin("*")
+@Tag(name = "Colunas", description = "Contém todas as operações relativas aos recursos para cadastro, edição e leitura de uma coluna.")
 public class ColunaController {
     private final ColunaService colunaService;
 
+    @Operation(
+            summary = "Criar uma nova coluna.",
+            description = "Recurso para criar uma nova coluna",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ColunaResponseDto.class)))
+            }
+    )
     @PostMapping
     public ResponseEntity<ColunaResponseDto> create(@RequestBody ColunaCreateDto createDto){
         Coluna newColuna = colunaService.criarColuna(ColunaMapper.toColuna(createDto));
