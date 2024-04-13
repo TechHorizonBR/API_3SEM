c7a4088fae982d73c63bf83fbfdaecab7a924b47
Luis Guimarães <luisegsilva.le@gmail.com>
2024-04-12 21:01:41 -0300

    feat: [NS-52] adicionando doc na classe de MetadataController

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/MetadataController.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/MetadataController.java
@@ -5,42 +5,93 @@ import com.api.nextschema.NextSchema.service.MetadataService;
 import com.api.nextschema.NextSchema.web.dto.MetadataCreateDto;
 import com.api.nextschema.NextSchema.web.dto.MetadataResponseDto;
 import com.api.nextschema.NextSchema.web.dto.mapper.MetadataMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/metadatas")
 @CrossOrigin("*")
+@Tag(name = "Metadatas", description = "Endpoints para fazer as operações com metadatas.")
 
 public class MetadataController {
     private final MetadataService metadataService;
 
+    @Operation(
+            summary = "Criar metadata.",
+            description = "Cria um novo metadata passando os parâmetros",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Metadata criado!",
+                        content = @Content(mediaType = "application/json"))
+            }
+    )
     @PostMapping
     public ResponseEntity<MetadataResponseDto> create(@RequestBody MetadataCreateDto metadadaCreateDto){
         Metadata metadata = metadataService.create(MetadataMapper.toMetadata(metadadaCreateDto));
         return ResponseEntity.ok().body(MetadataMapper.toDto(metadata));
     }
+
+    @Operation(
+            summary = "Listar metadatas",
+            description = "Lista todos os metadatas existentes",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Metadatas listados",
+                        content = @Content(mediaType = "application/json"))
+            }
+    )
     @GetMapping
     public ResponseEntity<List<MetadataResponseDto>> getAll(){
        List<Metadata> metadatas = metadataService.find();
         return ResponseEntity.ok().body(MetadataMapper.toListDto(metadatas));
     }
+
+    @Operation(
+            summary = "Deletar metadata por id",
+            description = "Endpoint para deletar o metadata passando um id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Metadata deletado",
+                        content = @Content(mediaType = "application/json"))
+            }
+    )
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         metadataService.deletebyId(id);
         return ResponseEntity.noContent().build();
     }
+
+    @Operation(
+            summary = "Buscar metadata",
+            description = "Buscar metadata passando um id específico",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Metadata encontrado",
+                    content = @Content(mediaType = "application/json"))
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<MetadataResponseDto> getById(@PathVariable Long id) {
         Metadata metadata = metadataService.findbyId(id);
         return ResponseEntity.ok().body(MetadataMapper.toDto(metadata));
     }
+
+    @Operation(
+            summary = "Buscar metadata por usuário",
+            description = "Retorna os metadatas por usuário",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Metadatas do usuário",
+                    content = @Content(mediaType = "apllication/json"))
+            }
+    )
     @GetMapping("/usuario")
     public ResponseEntity<MetadataResponseDto> getByUsuario(@RequestBody Usuario usuario){
         Metadata metadata = metadataService.findbyUsuario(usuario);
