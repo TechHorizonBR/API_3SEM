662a10b3d57dd3dc6f64c44a0fd49d812cd54e2a
santosjhony12 <santosdesouzajhony@gmail.com>
2024-04-16 21:31:22 -0300

    :feat: NS-119 Trabalhando com tratamento de erros de EntityNotFoundException em findById de Coluna

---
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.api.nextschema.NextSchema.web.exception;
+
+import com.api.nextschema.NextSchema.exception.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException ex,
+                                                                HttpServletRequest request){
+        log.error("API ERROR: ", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+}
