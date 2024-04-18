a62f785b17e6ea758724fb66a1046ba5d448e346
Amanda Vannucci <vannucci_job@hotmail.com>
2024-04-17 22:16:55 -0300

    :feat: NS-28, NS-110

--- Back-end/src/main/java/com/api/nextschema/NextSchema/entity/UsuarioEmpresa.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/entity/UsuarioEmpresa.java
@@ -1,2 +1,31 @@
-package com.api.nextschema.NextSchema.entity;public class UsuarioEmpresa {
+package com.api.nextschema.NextSchema.entity;
+
+import com.api.nextschema.NextSchema.enums.Role;
+import com.api.nextschema.NextSchema.web.dto.UsuarioDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UsuarioEmpresa {
+
+    @ManyToOne
+    private Usuario usuario;
+    @ManyToOne
+    private Empresa empresa;
+
+
+
+
 }
+
+
+
