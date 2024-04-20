a8035cd924caf9bb4b7a56eef87c3d4ca26607b3
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-04-19 21:29:06 -0300

    :feat: NS-91 concluindo validação

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/ColunaController.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/ColunaController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -42,7 +43,7 @@ public class ColunaController {
             }
     )
     @PostMapping
-    public ResponseEntity<ColunaResponseDto> create(@RequestBody ColunaCreateDto createDto){
+    public ResponseEntity<ColunaResponseDto> create(@Valid @RequestBody ColunaCreateDto createDto){
         Coluna newColuna = colunaService.criarColuna(ColunaMapper.toColuna(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(ColunaMapper.toDto(newColuna));
     }
