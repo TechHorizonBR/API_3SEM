f88652a6b5394785e8fe38ed66c83908d408a4b7
Amanda Vannucci <vannucci_job@hotmail.com>
2024-04-21 15:36:41 -0300

    :feat: NS-28 NS-110 Add inf in class UsuarioEmpresa

--- Back-end/src/main/java/com/api/nextschema/NextSchema/entity/UsuarioEmpresa.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/entity/UsuarioEmpresa.java
@@ -18,5 +18,40 @@ import org.springframework.beans.BeanUtils;
 
 
 public class UsuarioEmpresa {
+    private Usuario usuario;
+    private Empresa empresa;
+    private String cargo; // Pode ser útil incluir informações adicionais, como cargo ou papel do usuário na empresa
 
+    // Construtor
+    public UsuarioEmpresa(Usuario usuario, Empresa empresa, String cargo) {
+        this.usuario = usuario;
+        this.empresa = empresa;
+        this.cargo = cargo;
+    }
+
+    // Getters e Setters
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
 }
+
