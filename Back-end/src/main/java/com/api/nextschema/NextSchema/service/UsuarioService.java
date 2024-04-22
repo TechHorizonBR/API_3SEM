efa542ce5415571a4619b1f6199b406eb07179b1
gilvaneamaro <gilvane.amaro.jr@gmail.com>
2024-04-22 20:13:30 -0300

    :refactor: Ajustando importacoes

--- Back-end/src/main/java/com/api/nextschema/NextSchema/service/UsuarioService.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/service/UsuarioService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.List;
 
