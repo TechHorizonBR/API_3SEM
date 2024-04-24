e400f1fcd78a03a54d9e0f2a5943c0102114b189
andremeneses0103 <andremeneses0103@gmail.com>
2024-04-24 10:45:04 -0300

    :fix: NS-86 Duplo clique no botão de login corrigido

--- Front-end/Scripts/login.js
+++ Front-end/Scripts/login.js
@@ -1,39 +1,39 @@
 // document.addEventListener("DOMContentLoaded", function() {
-    // // Alerta ao abrir a página
-    // alert("Bem vindo ao NextSchema!");
+// // Alerta ao abrir a página
+// alert("Bem vindo ao NextSchema!");
 
-    // // Função a ser executada quando o botão for clicado
-    // document.getElementById("botao-entrar").onclick = function() {
-    //     // Captura o valor do campo de e-mail
-    //     let email = document.getElementById("texto-email").value;
-    //     // Exibe o e-mail no console
-    //     console.log(email);
-    // };
+// // Função a ser executada quando o botão for clicado
+// document.getElementById("botao-entrar").onclick = function() {
+//     // Captura o valor do campo de e-mail
+//     let email = document.getElementById("texto-email").value;
+//     // Exibe o e-mail no console
+//     console.log(email);
+// };
 
+document.getElementById("botaoEntrar").addEventListener("click", () => {
+    logar();
+});
 
+function logar() {
+    var textoEmail = document.getElementById("username").value;
+    var textoSenha = document.getElementById("password").value;
 
-        function logar(){
-            document.getElementById("botaoEntrar").onclick = function(){
-                var textoEmail = document.getElementById("username").value
-                var textoSenha = document.getElementById("password").value
-
-                if (textoEmail.trim() === "" && textoSenha.trim() === ""){
-                    alert("Por favor, preencha os campos.")
-                } else if (textoEmail.trim() === "") {
-                    alert("Por favor, insira o Email.");
-                    return;
-                } else if (textoSenha.trim() === "") {
-                    alert("Por favor, insira a Senha.");
-                    return;
-                } else if(textoEmail == "admin" && textoSenha == "admin"){
-                        location.href = "homeAdmin.html"
-                    } else if (textoEmail == "user@email.com" && textoSenha == "user" ) {
-                        location.href = "landing_zone/homeUser.html"
-                    }else{
-                    alert("Login inválido!")
-                        location.href = "index.html"
-                }
-            }
-        }
+    if (textoEmail.trim() === "" && textoSenha.trim() === "") {
+        alert("Por favor, preencha os campos.");
+    } else if (textoEmail.trim() === "") {
+        alert("Por favor, insira o Email.");
+        return;
+    } else if (textoSenha.trim() === "") {
+        alert("Por favor, insira a Senha.");
+        return;
+    } else if (textoEmail == "admin" && textoSenha == "admin") {
+        location.href = "homeAdmin.html";
+    } else if (textoEmail == "user@email.com" && textoSenha == "user") {
+        location.href = "landing_zone/homeUser.html";
+    } else {
+        alert("Login inválido!");
+        location.href = "index.html";
+    }
+}
 
 // });
