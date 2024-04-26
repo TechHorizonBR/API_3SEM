5b95408b5e21baf63b036e33eb9d05ebcdc1ddbf
BeatrizPlacido <biaplacsilva0212@gmail.com>
2024-04-26 02:28:38 -0300

    feat: NS-97 Add lógica para listar metadatas por empresa conforme o usuário

--- Front-end/Scripts/bronze/bz_visualizacao_metadata.js
+++ Front-end/Scripts/bronze/bz_visualizacao_metadata.js
@@ -1,1 +1,88 @@
-console.log("Deu bom!!")
+window.onload = () => {
+  getEmpresas();
+};
+
+const searchButton = document.querySelector("#btn-search");
+
+async function getEmpresas() {
+  try{
+    let usuario = {
+
+    };
+
+    let response = await axios.get(`http://localhost:8080/${usuario}`);
+    let empresas = response.data;
+
+    if(response.status === 200) {
+        generateOptions(empresas)
+    }else{
+        alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
+  }
+  }
+  catch(error){
+      console.error(error);
+  }
+};
+
+function generateOptions(empresas){
+  let select = document.getElementById("select-filter");
+
+  for(let i = 0; i < empresas.length; i++){
+    let selectOptions = `
+        <option class="option" id="select${i}" value="${empresas[i]}">${empresas[i]}</option>
+    `
+    select.insertAdjacentHTML("afterbegin", selectOptions);
+  }
+};
+
+searchButton.addEventListener("click", function () {
+  let selectValue = document.getElementById(`select-filter`).value;
+  getMetadata(selectValue);
+  generateList(metadatas, selectValue)
+});
+
+async function getMetadata(selectValue) {
+  try{
+    let empresa = {
+        nome: selectValue
+    };
+
+    let response = await axios.get(`http://localhost:8080/${empresa}`);
+    let metadatas = response.data;
+
+    if(response.status === 200) {
+        generateList(metadatas, selectValue);
+    }else{
+      alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
+  }
+
+  }
+  catch(error){
+      console.error(error);
+  }
+};
+
+function generateList(metadatas, selectValue) {
+  let listMetadatas = document.getElementById("listMetadatas");
+
+  for(let x = 0; x < empresas.length; x++){
+    let metadatasList = `
+        <div id="metadatas">
+            <div class="line">
+                  <div class="base-name">Base: </div>
+                  <div>${metadatas[x]}</div>
+            </div>
+            <div class="line">
+                  <div class="base-name">Empresa: </div>
+                  <div>${selectValue}</div>
+            </div>
+            <div class="viewMetadata">
+                  <i class="fa-solid fa-eye" id="view_eye"></i>
+                  <button class="cadastrarUsuario" id="view-metadata">VISUALIZAR</button>
+            </div>
+        </div>
+    `
+    listMetadatas.insertAdjacentHTML("afterbegin", metadatasList);
+  }
+
+}
