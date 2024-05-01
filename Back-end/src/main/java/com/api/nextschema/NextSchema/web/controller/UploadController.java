a75f7f2801a31d729bc4ed5c790e054e7c61206b
santosjhony12 <santosdesouzajhony@gmail.com>
2024-05-01 14:59:49 -0300

    :feat: NS-130 ajustando tratamento de erro

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/UploadController.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/UploadController.java
@@ -1,5 +1,8 @@
 package com.api.nextschema.NextSchema.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +19,25 @@ import java.util.List;
 @RequestMapping("/api/upload")
 @Slf4j
 @CrossOrigin("*")
+@Tag(name = "Upload", description = "Contém métodos relacionados a upload de arquivo CSV.")
 public class UploadController {
-
+    private static final long MAX_FILE_SIZE_BYTES = 1024L * 1024L * 1024L;
+    @Operation(
+            summary = "Recurso responsável por realizar o upload de arquivo CSV, retornando uma lista de nome de colunas e exemplo de dados.",
+            description = "Recurso para Upload",
+            responses = {
+                    @ApiResponse()
+            }
+    )
     @PostMapping
     public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("header") Boolean hasHeader){
         if(file.isEmpty()){
             return ResponseEntity.badRequest().body("Arquivo não pode estar vazio");
         }
+        if (file.getSize() > MAX_FILE_SIZE_BYTES) {
+            return ResponseEntity.badRequest().body("O tamanho do arquivo excede o limite permitido de 1 GB");
+        }
+
         try{
             BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()));
 
