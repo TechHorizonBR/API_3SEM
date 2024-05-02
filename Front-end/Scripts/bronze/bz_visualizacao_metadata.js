39bc8c790e9d58177bcf56a94761a43b3f2bf733
BeatrizPlacido <biaplacsilva0212@gmail.com>
2024-05-01 23:36:41 -0300

    feat: NS-97 Finalizando lógicas para visualização de metadatas para usuários bronze e lz

--- Front-end/Scripts/bronze/bz_visualizacao_metadata.js
+++ Front-end/Scripts/bronze/bz_visualizacao_metadata.js
@@ -13,13 +13,13 @@ const searchButton = document.querySelector("#btn-search");
 
 async function getEmpresas() {
   try{
-    let response = await axios.get(`http://localhost:8080/usuarioEmpresa/usuario/${userId}`);
-    let empresas = response.data;
+      let response = await axios.get(`http://localhost:8080/usuarioEmpresa/usuario/${userId}`);
+      let empresas = response.data;
 
-    if(response.status === 200) {
-        generateOptions(empresas)
-    }else{
-        alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
+      if(response.status === 200) {
+          generateOptions(empresas)
+      }else{
+          alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
   }
   }
   catch(error){
@@ -31,10 +31,10 @@ function generateOptions(empresas){
   let select = document.getElementById("select-filter");
 
   for(let i = 0; i < empresas.length; i++){
-    let selectOptions = `
-        <option class="option" id="select${i}" value="${empresas[i].id}">${empresas[i].nome}</option>
-    `
-    select.insertAdjacentHTML("afterbegin", selectOptions);
+      let selectOptions = `
+          <option class="option" onchange="" id="select${i}" value="${empresas[i].id}">${empresas[i].nome}</option>
+      `
+      select.insertAdjacentHTML("afterbegin", selectOptions);
   }
 };
 
@@ -45,14 +45,13 @@ searchButton.addEventListener("click", function () {
 
 async function getMetadata(selectValue) {
   try{
-    let response = await axios.get(`http://localhost:8080/${selectValue}`);
-    let metadatas = response.data;
-    console.log(metadatas)
+      let response = await axios.get(`http://localhost:8080/metadatas/empresa/${selectValue}`);
+      let metadatas = response.data;
 
-    if(response.status === 200) {
-        generateList(metadatas, selectValue);
-    }else{
-      alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
+      if(response.status === 200) {
+          generateList(metadatas, selectValue);
+      }else{
+          alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
   }
 
   }
@@ -62,34 +61,43 @@ async function getMetadata(selectValue) {
 };
 
 function generateList(metadatas, selectValue) {
-  let listMetadatas = document.getElementById("listMetadatas");
+    let listMetadatas = document.getElementById("listMetadatas");
 
-  if (metadatas.length === 0){
-    listMetadatas = `Não há metadatas disponíveis para essa empresa`
-    listMetadatas.insertAdjacentHTML("afterbegin", metadatasList);
-  }else{
-    for(let x = 0; x < listMetadatas.length; x++){
-      let metadatasList = `
-          <div id="metadatas">
-              <div class="line">
-                    <div class="base-name">Base: </div>
-                    <div>${metadatas[x]}</div>
-              </div>
-              <div class="line">
-                    <div class="base-name">Empresa: </div>
-                    <div>${selectValue}</div>
-              </div>
-              <div class="viewMetadata">
-                    <i class="fa-solid fa-eye" id="view_eye"></i>
-                    <button class="cadastrarUsuario" id="view-metadata">VISUALIZAR</button>
-              </div>
-          </div>
-      `
-      listMetadatas.insertAdjacentHTML("afterbegin", metadatasList);
+    listMetadatas.innerHTML = '';
+
+    if (metadatas.length === 0){
+        let metadatasList = `<h3 >Não há metadatas disponíveis para essa empresa</h3>`
+        listMetadatas.insertAdjacentHTML("afterbegin", metadatasList);
+    }else{
+        for(let x = 0; x < metadatas.length; x++){
+            let metadatasList = `
+                <div id="metadatas">
+                    <div class="line">
+                        <div class="base-name" id="name">Base: </div>
+                        <div>${metadatas[x].nome}</div>
+                    </div>
+                    <div class="line">
+                        <div class="base-name">Empresa: </div>
+                        <div>${selectValue}</div>
+                    </div>
+                    <div class="viewMetadata">
+                        <i class="fa-solid fa-eye" id="view_eye"></i>
+                        <button class="cadastrarUsuario" id="view-metadata" onclick="viewMetadata(${metadatas[x].id})">VISUALIZAR</button>
+                    </div>
+                </div>
+            `
+            listMetadatas.insertAdjacentHTML("afterbegin", metadatasList);
+        }
     }
-  }
+}
+
+function viewMetadata(metadata){
+    console.log(metadata)
+
+    localStorage.setItem("metadata", JSON.stringify(metadata));
+    window.location.href = "bz_validacao_metadata.html";
 }
 
 function updateNameUsuario(){
-  document.getElementById("username").innerHTML = userName
+    document.getElementById("username").innerHTML = userName
 }
--- Front-end/Scripts/landing_zone/homeUser.js
+++ Front-end/Scripts/landing_zone/homeUser.js
@@ -1,69 +1,103 @@
-let user = {
-    id: 1, 
-    nome: 'Jhony',
-    email: 'jhony@email.com',
-    senha: '123'
-}
-
-function getAllMetadatasporUsuario(url) {
-    fetch(url,{
-        method:"post",
-        mode: "cors",
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    .then(response => {return response.json()})
-    .then(data => {
-        console.log(data)
-        const metadataContainer = document.getElementById("metadataContainer");
-        if (data.length === 0){
-            console.log("Vazia")
-            const vazio = document.createElement("h1");
-            vazio.innerHTML = "Não há metadatas cadastrados. </br> Realize um upload para cadastrar."
-            metadataContainer.appendChild(vazio);
-        }else{
-            for (const key in data) {
-                if (Object.hasOwnProperty.call(data, key)) {
-                    const metadata = data[key];
-        
-                    const boxElement = document.createElement("div");
-            
-                    boxElement.classList.add("box");
-        
-                    const nameElement = document.createElement("h1");
-                    nameElement.textContent = metadata.nome;
-                    nameElement.classList.add("name-metadata");
-                    const idElement = document.createElement("p")
-                    idElement.textContent = metadata.id
-                    idElement.classList.add("id-metadata")
-                    idElement.style.display = "none"
-                    boxElement.appendChild(idElement);
-        
-                    boxElement.appendChild(nameElement);
-                    metadataContainer.appendChild(boxElement);
-                }
-            }
-        }
-    });
-}
-document.addEventListener("click", function(event) {
-
-    if (event.target.classList.contains("box")) {
-        const metadata = {
-            name: event.target.querySelector(".name-metadata").textContent,
-            id: event.target.querySelector(".id-metadata").textContent
-        };
-        localStorage.setItem("metadata", JSON.stringify(metadata));
-        window.location.href = "lz_resultado.html";
-    }
-});
-
-function updateNameUsuario(){
-    document.getElementById("username").innerHTML = user.nome
-}
-window.onload = function() {
-    getAllMetadatasporUsuario("http://localhost:8080/metadata/usuario");
+window.onload = () => {
+    getEmpresas();
     updateNameUsuario()
 };
+
+let usuario = localStorage.getItem("usuario");
+let userData = JSON.parse(usuario);
+
+let userId = userData.id;
+let userName = userData.nome;
+
+const searchButton = document.querySelector("#btn-search");
+
+async function getEmpresas() {
+    try{
+        let response = await axios.get(`http://localhost:8080/usuarioEmpresa/usuario/${userId}`);
+        let empresas = response.data;
+
+        if(response.status === 200) {
+            generateOptions(empresas)
+        }else{
+            alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
+    }
+    }
+    catch(error){
+        console.error(error);
+    }
+};
+
+function generateOptions(empresas){
+    let select = document.getElementById("select-filter");
+
+    for(let i = 0; i < empresas.length; i++){
+        let selectOptions = `
+            <option class="option" onchange="" id="select${i}" value="${empresas[i].id}">${empresas[i].nome}</option>
+        `
+        select.insertAdjacentHTML("afterbegin", selectOptions);
+    }
+};
+
+searchButton.addEventListener("click", function () {
+    let selectValue = document.getElementById(`select-filter`).value;
+    getMetadata(selectValue);
+});
+
+async function getMetadata(selectValue) {
+    try{
+        let response = await axios.get(`http://localhost:8080/metadatas/empresa/${selectValue}`);
+        let metadatas = response.data;
+
+        if(response.status === 200) {
+            generateList(metadatas, selectValue);
+        }else{
+            alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
+    }
+
+    }
+    catch(error){
+        console.error(error);
+    }
+};
+
+function generateList(metadatas, selectValue) {
+    let listMetadatas = document.getElementById("listMetadatas");
+
+    listMetadatas.innerHTML = '';
+
+    if (metadatas.length === 0){
+        let metadatasList = `<h3>Não há metadatas disponíveis para essa empresa</h3>`
+        listMetadatas.insertAdjacentHTML("afterbegin", metadatasList);
+    }else{
+        for(let x = 0; x < metadatas.length; x++){
+            let metadatasList = `
+                <div id="metadatas">
+                    <div class="line">
+                        <div class="base-name" id="name">Base: </div>
+                        <div>${metadatas[x].nome}</div>
+                    </div>
+                    <div class="line">
+                        <div class="base-name">Empresa: </div>
+                        <div>${selectValue}</div>
+                    </div>
+                    <div class="viewMetadata">
+                        <i class="fa-solid fa-eye" id="view_eye"></i>
+                        <button class="cadastrarUsuario" id="view-metadata" onclick="viewMetadata(${metadatas[x].id})">VISUALIZAR</button>
+                    </div>
+                </div>
+            `
+            listMetadatas.insertAdjacentHTML("afterbegin", metadatasList);
+        }
+    }
+}
+
+function viewMetadata(metadata){
+console.log(metadata)
+
+localStorage.setItem("metadata", JSON.stringify(metadata));
+window.location.href = "lz_resultado.html";
+}
+
+function updateNameUsuario(){
+    document.getElementById("username").innerHTML = userName
+}
