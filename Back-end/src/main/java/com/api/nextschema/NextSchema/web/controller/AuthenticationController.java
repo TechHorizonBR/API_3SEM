2c917f84c4db55b9d9598ef9be671e134fcf0da6
gilvaneamaro <gilvane.amaro.jr@gmail.com>
2024-05-24 21:16:27 -0300

    :fix: NS-153 Corrigido endpoint findUsuarioByEmail

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/AuthenticationController.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/AuthenticationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
