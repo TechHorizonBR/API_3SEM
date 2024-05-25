a15b4c7dd0dbd92b2be9817e95df4deb8f1a1404
santosjhony12 <santosdesouzajhony@gmail.com>
2024-05-24 22:42:40 -0300

    :feat: NS-158 Histórico finalizado

--- Back-end/src/main/java/com/api/nextschema/NextSchema/entity/Historico.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/entity/Historico.java
@@ -8,8 +8,10 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity
@@ -21,9 +23,8 @@ import java.time.LocalDateTime;
 public class Historico {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @CreatedDate
     @Column(name = "data_hora")
-    private LocalDateTime data_hora;
+    private LocalDateTime data_hora = LocalDateTime.now();
     @Column(name = "log")
     private String log;
     @CreatedBy
@@ -32,9 +33,22 @@ public class Historico {
     @ManyToOne
     private Metadata metadata;
 
-    public Historico(Metadata metadata, String log){
+    public Historico(Metadata metadata, String log, Usuario usuario){
         this.metadata = metadata;
         this.log = log;
+        this.usuario = usuario;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Historico historico = (Historico) o;
+        return Objects.equals(id, historico.id) && Objects.equals(data_hora, historico.data_hora) && Objects.equals(log, historico.log) && Objects.equals(usuario, historico.usuario) && Objects.equals(metadata, historico.metadata);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
