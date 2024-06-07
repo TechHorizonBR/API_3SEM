708a9a120662918385f7dc9bcbe15fe2c9feeda8
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-06-06 22:31:11 -0300

    feat: NS-166 endpoint finalizado

--- Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/DashController.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/web/controller/DashController.java
@@ -2,10 +2,14 @@ package com.api.nextschema.NextSchema.web.controller;
 
 import com.api.nextschema.NextSchema.service.DashService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RestController
@@ -14,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DashController {
     private final DashService dashService;
+
+    @GetMapping("/quantityTypeData")
+    public ResponseEntity<Map<String, Integer>> getQuantityTypeData(@RequestBody List<Long> idEmpresas){
+        return ResponseEntity.ok().body(dashService.getQuantityTypeData(idEmpresas));
+
+    }
 }
