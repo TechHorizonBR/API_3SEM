104d96c6c298709a493fcb93a7e3ebc4c3fe468c
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-06-06 22:10:35 -0300

    :feat: NS-166 elaborando condição

--- Back-end/src/main/java/com/api/nextschema/NextSchema/service/DashService.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/service/DashService.java
@@ -1,5 +1,6 @@
 package com.api.nextschema.NextSchema.service;
 
+import com.api.nextschema.NextSchema.entity.Coluna;
 import com.api.nextschema.NextSchema.entity.Metadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.ListableBeanFactory;
@@ -23,10 +24,25 @@ public class DashService {
 
     public List<Double>getQuantityTypeData(List <Long> ids){
         Map<String, Integer> quantityTypedata = new HashMap<>();
-        quantityTypedata.put("")
+        quantityTypedata.put("String", 0);
+        quantityTypedata.put("Int", 0);
+        quantityTypedata.put("Float", 0);
+        quantityTypedata.put("Boolean", 0);
+        quantityTypedata.put("Char", 0);
+        quantityTypedata.put("Date", 0);
         for(Long id : ids){
-            List<Metadata> metadata = metadataService.buscarPorEmpresa(id);
+            List<Metadata> metadatas = metadataService.buscarPorEmpresa(id);
+            for(Metadata metadata : metadatas){
+                List<Coluna> colunas = colunaService.buscarPorMetadata(metadata.getId());
+                for (Coluna coluna : colunas){
+                    switch (coluna.getTipo()){
+                        case "string" :
+                            quantityTypedata.put("String", quantityTypedata.get("String") +1);
+                            break;
 
+                    }
+                }
+            }
         }
 
 
