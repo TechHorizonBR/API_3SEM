614fada4a2648b572113008cb20f89b4354b6c4e
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-06-06 22:01:07 -0300

    :feat: NS-166 criando logica no DashService

--- Back-end/src/main/java/com/api/nextschema/NextSchema/service/DashService.java
+++ Back-end/src/main/java/com/api/nextschema/NextSchema/service/DashService.java
@@ -1,7 +1,13 @@
 package com.api.nextschema.NextSchema.service;
 
+import com.api.nextschema.NextSchema.entity.Metadata;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +20,18 @@ public class DashService {
     private final EmpresaService empresaService;
     private final UsuarioRoleAssociationService usuarioRoleAssociationService;
     private final UsuarioEmpresaService usuarioEmpresaService;
+
+    public List<Double>getQuantityTypeData(List <Long> ids){
+        Map<String, Integer> quantityTypedata = new HashMap<>();
+        quantityTypedata.put("")
+        for(Long id : ids){
+            List<Metadata> metadata = metadataService.buscarPorEmpresa(id);
+
+        }
+
+
+
+
+    }
+
 }
