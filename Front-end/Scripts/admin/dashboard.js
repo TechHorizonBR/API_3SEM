a110fa7da8885ad89bec2829954046a184234c12
BeatrizPlacido <biaplacsilva0212@gmail.com>
2024-06-12 21:48:37 -0300

    fix: NS-189 Correção das requisições

--- Front-end/Scripts/admin/dashboard.js
+++ Front-end/Scripts/admin/dashboard.js
@@ -2,9 +2,9 @@ window.onload = () => {
     opcoes_roles_metadata(roles,pagina_por_role,nome_por_role);
     getEmpresas();
     getMetadatas();
-    getTiposDeDados(0);
-    getStatusColuna(0);
-    getStatusMetadata(0);
+    getTiposDeDados(0, 0);
+    getStatusColuna(0, 0);
+    getestagioMetadatas(0);
 };
 
 let roles = JSON.parse(localStorage.getItem("roles"))
@@ -41,17 +41,17 @@ function opcoes_roles_metadata(roles,pagina_por_role,nome_por_role) {
         console.log("CHAVE:",pagina_por_role[1])
 
         if(roles[chave] == "ROLE_LZ"){
-            var listar_metadata = `
+            let listar_metadata = `
             <li><a href="${pagina_por_role[1]}">${nome_por_role[1]}</a></li>
         `;
             table.insertAdjacentHTML("beforeend", listar_metadata);
         }else if(roles[chave] == "ROLE_BRONZE"){
-            var listar_metadata = `
+            let listar_metadata = `
             <li><a href="${pagina_por_role[2]}">${nome_por_role[2]}</a></li>
         `;
             table.insertAdjacentHTML("beforeend", listar_metadata);
         }else if(roles[chave] == "ROLE_SILVER"){
-            var listar_metadata = `
+            let listar_metadata = `
             <li><a href="${pagina_por_role[3]}">${nome_por_role[3]}</a></li>
         `;
             table.insertAdjacentHTML("beforeend", listar_metadata);
@@ -66,14 +66,14 @@ function opcoes_roles_acoes(userData){
     let table = document.querySelector(".upload");
     for (let i in userData.roleUsuario){
         if (userData.roleUsuario[i] === "ROLE_LZ"){
-            var listar_metadata = `
+            let listar_metadata = `
             <li><a href="../landing_zone/lz_upload.html">Upload CSV</a></li>
         `;
         console.log(userData.roleUsuario)
         table.insertAdjacentHTML("beforeend", listar_metadata);
         }
         else if(userData.roleUsuario[i] === "ROLE_SILVER"){
-            var listar_metadata = `
+            let listar_metadata = `
             <li><a href="#">Relacionamentos</a></li>
         `;
         console.log(userData.roleUsuario)
@@ -130,7 +130,7 @@ async function getMetadatas(idEmpresa) {
         let metadatas = response.data;
 
         if(response.status === 200) {
-            generateOptionsMetadatas(metadatas)
+            generateOptionsMetadatas(metadatas, idEmpresa)
         }else{
             alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
         }
@@ -140,7 +140,7 @@ async function getMetadatas(idEmpresa) {
     }
 }
 
-function generateOptionsMetadatas(metadatas){
+function generateOptionsMetadatas(metadatas, idEmpresa){
     let select = document.getElementById("select-filter-metadata");
     let select_ = document.getElementById("filter-metadata");
 
@@ -162,24 +162,23 @@ function generateOptionsMetadatas(metadatas){
 
     select.addEventListener("change", function () {
         let selectValue = select.value;
-        getTiposDeDados(selectValue);
+        getTiposDeDados(selectValue, idEmpresa);
     });
     select_.addEventListener("change", function () {
         let selectValue = select_.value;
-        getStatusColuna(selectValue);
+        getStatusColuna(selectValue, idEmpresa);
     });
 }
 
-async function getStatusMetadata(idEmpresa) {
+async function getestagioMetadatas(idEmpresa) {
     try{
         let body = [idEmpresa]
 
-        let response = await api.get(`/dash/quantityByStage}`, body);
+        let response = await api.post(`/dash/quantityByStage`, body);
         let metadatas = response.data;
 
         if(response.status === 200) {
-            console.log(metadatas)
-            statusColuna(metadatas)
+            estagioMetadatas(metadatas)
         }else{
             alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
         }
@@ -189,12 +188,18 @@ async function getStatusMetadata(idEmpresa) {
     }
 }
 
-function statusMetadata(metadatas) {
-    var xValues = ["Pendente", "Invalidado", "Validado"];
-    var yValues = metadatas;
-    var barColors = ["#2b5797", "#e8c3b9", "#1e7145"];
+function estagioMetadatas(metadatas) {
+    const idCanva = Chart.getChart("estagioMetadatas")
 
-    new Chart("statusMetadata", {
+    if (idCanva) {
+        idCanva.destroy();
+    }
+
+    let xValues = ["LandingZone", "Bronze", "Silver"];
+    let yValues = [metadatas.LZ, metadatas.BRONZE, metadatas.SILVER];
+    let barColors = ["#2b5797", "#e8c3b9", "#1e7145"];
+
+    new Chart("estagioMetadatas", {
         type: "doughnut",
         data: {
             labels: xValues,
@@ -222,10 +227,11 @@ async function getTiposDeDados(idEmpresa) {
     try{
         let body = [idEmpresa]
 
-        let response = await api.get(`/metadatas/empresa/${idEmpresa}`, body);
+        let response = await api.post(`/dash/quantityTypeData`, body);
         let dadosEmpresa = response.data;
 
         if(response.status === 200) {
+            console.log(dadosEmpresa)
             tipos_de_dados(dadosEmpresa)
         }else{
             alert("Um erro ocorreu no sistema, tente novamente mais tarde.")
@@ -237,6 +243,12 @@ async function getTiposDeDados(idEmpresa) {
 }
 
 function tipos_de_dados(dadosEmpresa) {
+    const idCanva = Chart.getChart("myChart")
+
+    if (idCanva) {
+        idCanva.destroy();
+    }
+
     let xValues = ["Float", "String", "Integer", "Boolean", "Char", "Date"];
     let yValues = dadosEmpresa;
     let barColors = ["#94C2FF", "#67FECB", "#8FE3FD", "#FECD00", "#A273FF", "#0299FE"];
@@ -260,9 +272,13 @@ function tipos_de_dados(dadosEmpresa) {
     });
 }
 
-async function getStatusColuna(idEmpresa) {
+async function getStatusColuna(idMetadata, idEmpresa) {
     try{
-        let response = await api.get(`/metadatas/empresa/${idEmpresa}`);
+        let body = [
+            idEmpresa
+        ]
+
+        let response = await api.post(`/dash/quantityStatus/${idMetadata}`, body);
         let metadatas = response.data;
 
         if(response.status === 200) {
@@ -276,12 +292,20 @@ async function getStatusColuna(idEmpresa) {
     }
 }
 
-function statusColuna() {
-    var xValues = ["Pendente", "Invalidado", "Validado"];
-    var yValues = [25, 35, 40];
-    var barColors = ["#b91d47", "#00aba9", "#2b5797"];
+function statusColuna(metadatas) {
+    const idCanva = Chart.getChart("statusColuna")
 
-    new Chart("statusColuna", {
+    if (idCanva) {
+        idCanva.destroy();
+    }
+
+    let xValues = ["Pendente", "Invalidado", "Validado"];
+    let yValues = [metadatas.PENDENTE, metadatas.INVALIDADO, metadatas.VALIDADO];
+    let barColors = ["#b91d47", "#00aba9", "#2b5797"];
+
+    let canva = document.getElementById("statusColuna").getContext("2d")
+
+    const myChart = new Chart(canva, {
     type: "doughnut",
     data: {
         labels: xValues,
